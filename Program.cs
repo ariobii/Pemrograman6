0b8a5653ac8f4a8c85d234449f665376c940d87f
ariobii <auliabimo5@gmail.com>
2019-04-09 14:55:15 +0700

    sudah bosan

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace sisapOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //new instance
+            Time objekTime = new Time(1,2,3,4,5,6);
+
+            //panggil method
+            //objekTime.DisplayCurrentTime();
+
+            /*
+            objekTime.Year = 2019;
+            objekTime.Month = 4;
+            objekTime.Date = 9;
+            */
+           
+            Console.WriteLine("Year : {0}", objekTime.Year.ToString());
+            Console.WriteLine("Month : {0}", objekTime.Month.ToString());
+            Console.WriteLine("Date : {0}", objekTime.Date.ToString());
+            Console.WriteLine("Hour : {0}", objekTime.Hour);
+            Console.WriteLine("Minute : {0}", objekTime.Minute);
+            Console.WriteLine("Second : {0}", objekTime.Second);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Time.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace sisapOOP
+{
+    class Time
+    {
+        //field
+        private int year;
+        private int month;
+        private int date;
+        private int hour;
+        private int minute;
+        private int second;
+
+        //properties
+        public int Year {
+            get { return year; }
+            set { year = value; }
+        }
+
+        public int Month {
+            get { return month; }
+            set { month = value; }
+        }
+
+        public int Date {
+            get { return date; }
+            set { date = value; }
+        }
+
+        public int Hour {
+            get { return hour; }
+            set { hour = value; }
+        }
+
+        public int Minute {
+            get { return minute; }
+            set { minute = value; }
+        }
+        public int Second {
+            get { return second; }
+            set { second = value; }
+        }
+
+        //constructor
+        public Time()
+        {
+            year = 2019;
+            month = 4;
+            date = 9;
+            hour = 14;
+            minute = 35;
+            second = 12;
+        }
+
+        //overlord constructor
+        public Time(int year, int month, int date, int hour, int minute, int second)
+        {
+            this.year = year;
+            this.month = month;
+            this.date = date;
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
+
+        //public methods
+        public void DisplayCurrentTime()
+        {
+            Console.WriteLine("Waktu Sekarang : {0}", DateTime.Now.ToString());
+        }
+    }
+}
